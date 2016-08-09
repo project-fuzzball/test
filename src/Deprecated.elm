e8b3c30b681c1b18a0130968dac4ed8798df04bf
Richard Feldman <richard.t.feldman@gmail.com>
2016-08-08 23:13:38 -0700

    Deprecate everything. (#18)

---
+++ src/Deprecated.elm
@@ -0,0 +1,12 @@
+module Deprecated exposing (deprecated)
+
+{-| DEPRECATED - This has been replaced by elm-community/elm-test
+@docs deprecated
+-}
+
+
+{-| DEPRECATED - This has been replaced by elm-community/elm-test
+-}
+deprecated : String
+deprecated =
+    "DEPRECATED - This has been replaced by elm-community/elm-test"
